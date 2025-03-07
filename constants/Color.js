7f7befb4fdb60dc6c79b21f152a3d8f1926b0d9d
atmaniali <atmaniali97@gmail.com>
2025-03-07 22:34:01 +0100

    reset project default file nd adding some icons and images to start project

---
+++ constants/Color.js
@@ -0,0 +1,10 @@
+export const Colors = {
+    blackGray: '#242C3B',
+    bleuLight: '#37B6E9',
+    purple: '#4B4CED',
+    gray: '#353F54',
+    white: '#fff',
+    black: '#000',
+    blackLight: '#222834'
+}
+
